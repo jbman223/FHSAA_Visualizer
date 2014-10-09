77b79f4afdab4161306576e1024b211523457237
Jacob Buckheit <jbman223@gmail.com>
2014-10-09 14:55:16 -0400

    Update result outputter to proper result outputting for swimmers who have made it all the way through.

--- api/outputResults.php
+++ api/outputResults.php
@@ -2,8 +2,8 @@
 require_once "require.php";
 
 echo "<html><body><pre>";
-$state = $db->prepare("SELECT `final_time` FROM swim_information WHERE event_name = ? AND meet_type = ?");
-$state->execute(array("Event 8  Boys 50 Yard Freestyle", "Districts"));
+$state = $db->prepare("SELECT `final_time` FROM swim_information WHERE event_name = ? AND meet_type = ? AND meet_name LIKE ?");
+$state->execute(array("Event 8  Boys 50 Yard Freestyle", "States", "%1A%"));
 foreach ($state->fetchAll() as $swim) {
     if ($swim[0] != 0)
         echo $swim[0]."\n";
