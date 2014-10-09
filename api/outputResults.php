02b454284dad6cc15a8293432c4c3ad25d40ef58
Jacob Buckheit <jbman223@gmail.com>
2014-10-08 20:05:49 -0400

    Add result outputter for getting times to compute statistics.

--- api/outputResults.php
+++ api/outputResults.php
@@ -1,10 +1,11 @@
 <?php
 require_once "require.php";
 
-echo "<pre>";
+echo "<html><body><pre>";
 $state = $db->prepare("SELECT `final_time` FROM swim_information WHERE event_name = ? AND meet_type = ?");
 $state->execute(array("Event 8  Boys 50 Yard Freestyle", "Districts"));
 foreach ($state->fetchAll() as $swim) {
-    echo $swim[0];
+    if ($swim[0] != 0)
+        echo $swim[0]."\n";
 }
-echo "</pre>";
+echo "</pre></body></html>";
