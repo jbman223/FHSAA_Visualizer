7d7cfc1eff343c5ec9894d6e49da11a9d9db6236
Jacob Buckheit <jbman223@gmail.com>
2014-10-09 19:55:11 -0400

    Implement statistics funcitons into output results.

--- api/outputResults.php
+++ api/outputResults.php
@@ -1,7 +1,10 @@
 <?php
 require_once "require.php";
+require_once "statisticalFunctions.php";
 
 echo "<html><body><pre>";
+$x = array();
+$y = array();
 $state = $db->prepare("SELECT `final_time`, `swimmer_id` FROM swim_information WHERE event_name = ? AND meet_type = ? AND meet_title LIKE ?");
 $state->execute(array("Event 8  Boys 50 Yard Freestyle", "States", "%1A%"));
 $states = $state->fetchAll();
@@ -11,9 +14,13 @@ foreach ($states as $swim) {
     if ($swim[0] != 0) {
         $regionalQuery->execute(array("Event 8  Boys 50 Yard Freestyle", "Regionals", $swim["swimmer_id"]));
         $regions = $regionalQuery->fetch()["final_time"];
+        array_push($x, $regions);
+        array_push($y, $swim["final_time"]);
         $districtQuery->execute(array("Event 8  Boys 50 Yard Freestyle", "Districts", $swim["swimmer_id"]));
         $districts = $districtQuery->fetch()["final_time"];
         echo $swim["final_time"]."\t".$regions."\t".$districts."\n";
     }
 }
+echo "\n\n\n";
+var_dump(linear_regression($x, $y));
 echo "</pre></body></html>";
--- api/statisticalFunctions.php
+++ api/statisticalFunctions.php
@@ -1,11 +1,10 @@
 <?php
-function linear_regression($x, $y) {
+function linear_regression($x, $y)
+{
     $n = count($x);
 
     if ($n != count($y)) {
-
         trigger_error("linear_regression(): Number of elements in coordinate arrays do not match.", E_USER_ERROR);
-
     }
 
     $x_sum = array_sum($x);
@@ -14,13 +13,13 @@ function linear_regression($x, $y) {
     $xx_sum = 0;
     $xy_sum = 0;
 
-    for($i = 0; $i < $n; $i++) {
-        $xy_sum+=($x[$i]*$y[$i]);
-        $xx_sum+=($x[$i]*$x[$i]);
+    for ($i = 0; $i < $n; $i++) {
+        $xy_sum += ($x[$i] * $y[$i]);
+        $xx_sum += ($x[$i] * $x[$i]);
     }
 
     $m = (($n * $xy_sum) - ($x_sum * $y_sum)) / (($n * $xx_sum) - ($x_sum * $x_sum));
     $b = ($y_sum - ($m * $x_sum)) / $n;
 
-    return array("a"=>$m, "b"=>$b);
+    return array("a" => $m, "b" => $b);
 }
