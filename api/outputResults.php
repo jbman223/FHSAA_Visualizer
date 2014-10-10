9603b14635797da55cb28b1a7feef078c45437a0
Jacob Buckheit <jbman223@gmail.com>
2014-10-09 20:49:04 -0400

    Final implementation of statistics. Adjusting of variable names to generalize and make code more understandable.

--- api/outputResults.php
+++ api/outputResults.php
@@ -4,10 +4,8 @@ require_once "statisticalFunctions.php";
 
 echo "<html><body><pre>";
 $eventName = "Event 16  Boys 500 Yard Freestyle";
-//resp var
 $startingMeet = "States";
-//exp var
-$secondMeet = "Regions";
+$secondMeet = "Regionals";
 $class = "%1A%";
 $x = array();
 $y = array();
@@ -18,11 +16,11 @@ $secondaryQuery = $db->prepare("SELECT `final_time`, `swimmer_id` FROM swim_info
 foreach ($states as $swim) {
     if ($swim[0] != 0) {
         $secondaryQuery->execute(array($eventName, $secondMeet, $swim["swimmer_id"]));
-        $secondary = $secondaryQuery->fetch();
-        if (count($secondary) == 1) {
-            array_push($x, $secondary["final_time"]);
+        $second = $secondaryQuery->fetchAll();
+        if (count($second) > 0) {
+            array_push($x, $second[0]["final_time"]);
             array_push($y, $swim["final_time"]);
-            echo $secondary["final_time"] . "\t" . $swim["final_time"] . "\n";
+            echo $second[0]["final_time"] . "\t" . $swim["final_time"] . "\n";
         }
     }
 }
